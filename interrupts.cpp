8807d00af39014d0b4b5da38904b8339e4094be7
Tony <tony.yao23@ycdsbk12.ca>
2025-09-28 21:41:41 -0400

    added cpu logging and general intr logging

--- interrupts.cpp
+++ interrupts.cpp
@@ -2,25 +2,26 @@
  *
  * @file interrupts.cpp
  * @author Sasisekhar Govind
+ * @author Tony Yao
  *
  */
 
-#include<interrupts.hpp>
+#include "interrupts.hpp"
 
 int main(int argc, char** argv) {
 
     //vectors is a C++ std::vector of strings that contain the address of the ISR
-    //delays  is a C++ std::vector of ints that contain the delays of each device
+    //delays is a C++ std::vector of ints that contain the delays of each device
     //the index of these elemens is the device number, starting from 0
     auto [vectors, delays] = parse_args(argc, argv);
     std::ifstream input_file(argv[1]);
-
     std::string trace;      //!< string to store single line of trace file
     std::string execution;  //!< string to accumulate the execution output
 
     /******************ADD YOUR VARIABLES HERE*************************/
-
-
+    int current_time = 0;
+    int context_save_time = 10;
+    int cpu_end_time = 0;
 
     /******************************************************************/
 
@@ -29,8 +30,19 @@ int main(int argc, char** argv) {
         auto [activity, duration_intr] = parse_trace(trace);
 
         /******************ADD YOUR SIMULATION CODE HERE*************************/
+        if (activity == "CPU") {
+            execution += std::to_string(current_time) + ", " + std::to_string(duration_intr) + ", CPU execution\n";
+            current_time += duration_intr;
+        }
+        else { 
+            auto [intr_log, updated_time] = intr_boilerplate(current_time, duration_intr, context_save_time, vectors);
+            execution += intr_log;
+            current_time = updated_time;
 
+            execution += std::to_string(current_time) + ", " + std::to_string(1) + ", IRET\n";
+            current_time++;
 
+        }
 
         /************************************************************************/
 
