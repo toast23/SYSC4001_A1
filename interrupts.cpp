66bc96ec32ec03a59f1c8989bdf88847cae10841
austinWXH2004 <austinhanwenxuan@outlook.com>
2025-10-05 00:56:30 -0400

    update interrupts.cpp

--- interrupts.cpp
+++ interrupts.cpp
@@ -10,109 +10,107 @@
 #include "interrupts.hpp"
 #include <tuple>
 
+int main(int argc, char **argv)
+{
 
-int main(int argc, char** argv) {
+    std::cout << "hello world" << std::endl;
 
-    std::cout << "hello world" << std::endl; 
-
-    //vectors is a C++ std::vector of strings that contain the address of the ISR
-    //delays is a C++ std::vector of ints that contain the delays of each device
-    //the index of these elemens is the device number, starting from 0
+    // vectors is a C++ std::vector of strings that contain the address of the ISR
+    // delays is a C++ std::vector of ints that contain the delays of each device
+    // the index of these elemens is the device number, starting from 0
     auto [vectors, delays] = parse_args(argc, argv);
     std::ifstream input_file(argv[1]);
-    std::string trace;      //!< string to store single line of trace file
-    std::string execution;  //!< string to accumulate the execution output
+    std::string trace;     //!< string to store single line of trace file
+    std::string execution; //!< string to accumulate the execution output
 
     /******************ADD YOUR VARIABLES HERE*************************/
     int current_time = 0;
     int context_save_time = 10;
     int cpu_end_time = 0;
-    int each_activity_within_ISR = 110; 
+    int each_activity_within_ISR = 110;
     /******************************************************************/
 
-    
-    
-    //parse each line of the input trace file
-    while(std::getline(input_file, trace)) {
+    // parse each line of the input trace file
+    while (std::getline(input_file, trace))
+    {
         auto [activity, duration_intr] = parse_trace(trace);
 
         /******************ADD YOUR SIMULATION CODE HERE*************************/
 
-        if (activity == ""){
+        if (activity == "")
+        {
             continue;
         }
 
-        if (activity == "CPU") {
+        if (activity == "CPU")
+        {
             execution += std::to_string(current_time) + ", " + std::to_string(duration_intr) + ", CPU Burst\n";
             current_time += duration_intr;
         }
         else if (activity == "SYSCALL")
         {
             auto [new_execution, updated_time] = intr_boilerplate(current_time, duration_intr, context_save_time, vectors);
-            execution += new_execution; 
+            execution += new_execution;
             current_time = updated_time;
 
+            int amount_activity_ISR = 0;
+            execution += std::to_string(current_time) + ", " + std::to_string(each_activity_within_ISR) + ", SYSCALL: run the ISR (device driver)\n";
+            current_time += each_activity_within_ISR;
+            amount_activity_ISR++;
 
-            int amount_activity_ISR = 0; 
-            execution += std::to_string(current_time) + ", " + std::to_string(each_activity_within_ISR) + ", SYSCALL: run the ISR (device driver)\n"; 
-            current_time += each_activity_within_ISR; 
-            amount_activity_ISR++; 
+            execution += std::to_string(current_time) + ", " + std::to_string(each_activity_within_ISR) + ", transfer data from device to memory\n";
+            current_time += each_activity_within_ISR;
+            amount_activity_ISR++;
 
-            execution += std::to_string(current_time) + ", " + std::to_string(each_activity_within_ISR) + ", transfer data from device to memory\n"; 
-            current_time += each_activity_within_ISR; 
-            amount_activity_ISR++; 
-
-            int remain_activity_time_within_ISR; 
+            int remain_activity_time_within_ISR;
             if (duration_intr < 0 || duration_intr >= delays.size())
             {
                 continue;
-            } 
-            
-            remain_activity_time_within_ISR = delays.at(duration_intr) - (amount_activity_ISR * each_activity_within_ISR); 
-            if (remain_activity_time_within_ISR < 0)
-            {
-                 remain_activity_time_within_ISR = 0; 
             }
 
-            execution += std::to_string(current_time) + ", " + std::to_string(remain_activity_time_within_ISR) + ", check for errors\n"; 
-            current_time += remain_activity_time_within_ISR;
+            remain_activity_time_within_ISR = delays.at(duration_intr) - (amount_activity_ISR * each_activity_within_ISR);
+            if (remain_activity_time_within_ISR < 0)
+            {
+                remain_activity_time_within_ISR = 0;
+            }
 
+            execution += std::to_string(current_time) + ", " + std::to_string(remain_activity_time_within_ISR) + ", check for errors\n";
+            current_time += remain_activity_time_within_ISR;
         }
-        else if (activity == "END_IO") {
+        else if (activity == "END_IO")
+        {
 
             auto [new_execution, updated_time] = intr_boilerplate(current_time, duration_intr, context_save_time, vectors);
             execution += new_execution;
-            current_time = updated_time; 
+            current_time = updated_time;
 
+            int amount_activity = 0;
 
-            int amount_activity = 0; 
-
-            execution += std::to_string(current_time) + ", " + std::to_string(each_activity_within_ISR) + ", END_IO: run the ISR(device driver)\n"; 
-            current_time += each_activity_within_ISR; 
+            execution += std::to_string(current_time) + ", " + std::to_string(each_activity_within_ISR) + ", END_IO: run the ISR(device driver)\n";
+            current_time += each_activity_within_ISR;
             amount_activity++;
 
             if (duration_intr < 0 || duration_intr >= delays.size())
             {
                 continue;
             }
-            int device_number_delay_time = delays.at(duration_intr); 
+            int device_number_delay_time = delays.at(duration_intr);
             int remain_time_within_ISR = device_number_delay_time - (amount_activity * each_activity_within_ISR);
 
-            if(remain_time_within_ISR < 0)
+            if (remain_time_within_ISR < 0)
             {
-                remain_time_within_ISR = 0; 
+                remain_time_within_ISR = 0;
             }
 
-            execution += std::to_string(current_time) + ", " + std::to_string(remain_time_within_ISR) + ", check device status\n"; 
-            current_time += remain_time_within_ISR; 
-            
+            execution += std::to_string(current_time) + ", " + std::to_string(remain_time_within_ISR) + ", check device status\n";
+            current_time += remain_time_within_ISR;
         }
-        else { 
-             std::cerr << "error is happened here so nothing to do unless OS let u to do work" << std::endl; 
+        else
+        {
+            std::cerr << "error is happened here so nothing to do unless OS let u to do work" << std::endl;
         }
 
         /************************************************************************/
-
     }
 
     input_file.close();
